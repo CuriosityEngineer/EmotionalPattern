d4cfdc6d2dcf54cdc4c93da9d7d76aa5829d2528
CuriosityEngineer <aeshta@gmail.com>
2016-06-11 14:01:26 +0200

    List of data (emotions)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,91 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Emotion.create({group_emotion:"AFFECTIONATE", emotion: "compassionate", color:"#8CFF8C"})
-Emotion.create({group_emotion:"ENGAGED", emotion:"alert", color:"#FFC48C"})
+Emotion.create({group_emotion:"love", emotion: "Compassion", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Affection", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Adoration", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Fondness", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Love", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Liking", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Attraction", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Caring", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Tenderness", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Sentimentaliy", color:"yellow"})
+Emotion.create({group_emotion:"love", emotion: "Arousal", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Desire", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Lust", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Passion", color:"green"})
+Emotion.create({group_emotion:"love", emotion: "Longing", color:"yellow"})
+Emotion.create({group_emotion:"Joy", emotion: "Joy", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Cheerfull", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Amusement", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Bliss", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Gaiety", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Glee", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Jolliness", color:"red"})
+Emotion.create({group_emotion:"Joy", emotion: "Joviality", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Delight", color:"red"})
+Emotion.create({group_emotion:"Joy", emotion: "Enjoy", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Glad", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Happiness", color:"red"})
+Emotion.create({group_emotion:"Joy", emotion: "Jubilation", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "Elation", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "Satisfaction", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "ectastasy", color:"red"})
+Emotion.create({group_emotion:"Joy", emotion: "euphoria", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "Enthusiasm", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "Zeal", color:"pink"})
+Emotion.create({group_emotion:"Joy", emotion: "Zest", color:"red"})
+Emotion.create({group_emotion:"Joy", emotion: "Excitement", color:"orange"})
+Emotion.create({group_emotion:"Joy", emotion: "Thrill", color:"red"})
+Emotion.create({group_emotion:"Surprise", emotion: "Exhilaration", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "Contentment", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "Pleasure", color:"red"})
+Emotion.create({group_emotion:"Surprise", emotion: "Pride", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "Optimism", color:"brown"})
+Emotion.create({group_emotion:"Surprise", emotion: "Triumph", color:"brown"})
+Emotion.create({group_emotion:"Surprise", emotion: "Eagerness", color:"brown"})
+Emotion.create({group_emotion:"Surprise", emotion: "Hope", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "Enthrallment", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "relief", color:"orange"})
+Emotion.create({group_emotion:"Surprise", emotion: "amazement", color:"cyan"})
+Emotion.create({group_emotion:"Surprise", emotion: "Surprise", color:"cyan"})
+Emotion.create({group_emotion:"Surprise", emotion: "astonishment", color:"cyan"})
+Emotion.create({group_emotion:"Anger", emotion: "Aggravation", color:"red"})
+Emotion.create({group_emotion:"Anger", emotion: "anger", color:"red"})
+Emotion.create({group_emotion:"Anger", emotion: "rage", color:"red"})
+Emotion.create({group_emotion:"Anger", emotion: "irritation", color:"red"})
+Emotion.create({group_emotion:"Anger", emotion: "Scorn", color:"red"})
+Emotion.create({group_emotion:"Sadness", emotion: "Sadness", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Depression", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Grief", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Gloom", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Hopelessness", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Sorrow", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Dissappiontment", color:"blue"})
+Emotion.create({group_emotion:"Sadness", emotion: "Loneliness", color:"blue"})
+Emotion.create({group_emotion:"Fear", emotion: "Alarm", color:"purple"})
+Emotion.create({group_emotion:"Fear", emotion: "Horror", color:"purple"})
+Emotion.create({group_emotion:"Fear", emotion: "Shock", color:"purple"})
+Emotion.create({group_emotion:"Fear", emotion: "Fear", color:"purple"})
+Emotion.create({group_emotion:"Fear", emotion: "panic", color:"purple"})
+Emotion.create({group_emotion:"Fear", emotion: "Anxity", color:"purple"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Emotion.create({group_emotion:"Joy", emotion: "Rapture", color:"red"})
